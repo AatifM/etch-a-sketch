6b81f4227969877f21492fff4cf32a667cbd46a7
AatifM <aatifmb@gmail.com>
2026-04-08 20:01:01 -0400

    Added mouse event for grid

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 const container = document.querySelector(".container");
+const button = document.querySelector(".size-button");
 
 function generateDivs(rows, cols) {
     for (let i = 0; i < rows * cols; i++) {
         let div = document.createElement("div");
         container.appendChild(div);
+        div.addEventListener("mouseenter", (event) => {
+            event.target.classList.add("hovered");
+        });
     }
 }
 
-document.addEventListener("DOMContentLoaded", (event) => {
+document.addEventListener("DOMContentLoaded", () => {
     generateDivs(16, 16);
+});
+
+button.addEventListener("click", () => {
+    window.prompt("Enter grid length");
 });
